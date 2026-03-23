508c5dccb6dcaa2bbc4145d46e967c7cf7038d92
novotnypope <novotnypope@gmail.com>
2026-03-23 19:55:31 -0400

    add orders router and app middleware setup

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 import express from "express";
+import usersRouter from "#routes/users";
+import productsRouter from "#routes/products";
+import ordersRouter from "#routes/orders";
+
 const app = express();
+
+app.use(express.json());
+
+app.use("/users", usersRouter);
+app.use("/products", productsRouter);
+app.use("/orders", ordersRouter);
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({ error: "Internal server error" });
+});
+
 export default app;
